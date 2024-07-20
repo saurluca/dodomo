bd61cf9aa145c433703868051668148d382cbd30
saurluca <spuckteller@gmail.com>
2024-07-20 23:05:12 +0200

    added meme_gen sample file
    Signed-off-by: saurluca <spuckteller@gmail.com>

---
+++ backend/extras/meme_gen.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+
+response = client.images.generate(
+    model="dall-e-3",
+    prompt="A funny cat meme about clothes",
+    n=1,
+    size="1024x1024",
+    style="natural",
+)
+
+print(response)
