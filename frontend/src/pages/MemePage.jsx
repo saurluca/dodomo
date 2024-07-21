29c6573cb613ea2a08656a45547391d10a292ff8
saurluca <spuckteller@gmail.com>
2024-07-21 14:14:33 +0200

    removed now unused meme button
    Signed-off-by: saurluca <spuckteller@gmail.com>

--- frontend/src/pages/MemePage.jsx
+++ frontend/src/pages/MemePage.jsx
@@ -1,28 +1,13 @@
 import {Button} from "@/components/ui/button.jsx";
 import {useState} from "react";
-import axios from "axios";
 import LoadingSpinner from "@/components/ui/loadingSpinner.jsx";
 import {Input} from "@/components/ui/input.jsx";
 import {getMeme} from "@/services/memeApi.jsx";
 
 const MemePage = () => {
-    // State for storing the image source URL
     const [imageSrc, setImageSrc] = useState("/lol.png")
-    // State to track loading status
     const [loading, setLoading] = useState(false)
     const [prompt, setPrompt] = useState("")
-
-    const handleClick = async () => {
-        // Set loading to true at the start of the API call
-        setLoading(true)
-        console.log("loading Meme")
-        // Perform the API call to fetch a meme
-        const response = await axios.get("https://lucasaur.com/api/meme")
-        // Update the image source state with the fetched URL
-        setImageSrc(response.data[1][1][0][2][1])
-        // Set loading to false after the API call is complete
-        setLoading(false)
-    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -40,22 +25,19 @@ const MemePage = () => {
     }
 
     return (
-        <div className="w-full h-full">
-            <form onSubmit={handleSubmit} className="flex max-w-[420px] w-full items-center space-x-2">
+        <div className>
+            <form onSubmit={handleSubmit} className="flex w-full items-center space-x-2">
+                <Button className="text-4xl hover:bg-green-700 w-80 h-16" type="submit">MEME THIS</Button>
                 <Input
                     type="text"
                     placeholder="New Task"
                     value={prompt}
                     onChange={(e) => setPrompt(e.target.value)}
-                    className="bg-white shadow"
+                    className="bg-white shadow text-2xl h-16 p-4 rounded-md"
                     required
                 />
-                <Button className="shadow border hover:bg-green-700" type="submit">MEME THIS</Button>
             </form>
-            <div className="flex justify-center">
-                {/* Button to trigger meme loading */}
-                <Button className="text-6xl  hover:bg-green-700 w-100 h-20" onClick={handleClick}>SHOW MEME</Button>
-            </div>
+            <br/>
             <div className="flex justify-content">
                 {/* Conditional rendering: show loading spinner or image based on loading state */}
                 {loading ? <LoadingSpinner/> : <img id="image" src={imageSrc} alt="Meme" width="800" height="800"/>}
