6a30d6ae0c4d79470939f9b822950c1fb9e9d306
saurluca <spuckteller@gmail.com>
2024-07-21 14:14:16 +0200

    simplifed loading spinner, to not be automaitcally in middle of page
    Signed-off-by: saurluca <spuckteller@gmail.com>

--- frontend/src/components/ui/loadingSpinner.jsx
+++ frontend/src/components/ui/loadingSpinner.jsx
@@ -1,10 +1,10 @@
 const LoadingSpinner = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
+        // <div className="flex justify-center items-center h-screen">
             <div className="spinner-border animate-spin inline-block w-32 h-32 border-4 rounded-full text-2xl" role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
-        </div>
+        // </div>
     );
 };
 
